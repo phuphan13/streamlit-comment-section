18f746ad5eda7aedba2f829c8cb5c6b6e4d40e7c
Phu Phan <89576717+phuphan13@users.noreply.github.com>
2023-07-26 16:13:05 +0930

    Add files via upload

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import pandas as pd
 from datetime import datetime
 
 def main():
-    
+    #writing chart title 
     st.title('Monthly sales analysis')
     st.write('')
     
+    #creating data frame for monthly sales
     df = pd.DataFrame({'Month' : ['Jan', 'Feb', 'Mar','Apr', 'May','Jun'],
                         'Sales': [1000, 2000, 1540, 1700,1320, 1120]})
     
+    #rendering dataframe on the left and chart on the right panel
     col1, col2 = st.columns([1,3])
-    
     with col1:
         st.dataframe(df)
-        
     with col2:
         st.bar_chart(df,x='Month',y='Sales')
         
@@ -23,10 +23,11 @@ def main():
     
     df_comment = None
     
-    #Loading comments file
+    #check if 'comment' is not existed in session state then load csv and store in the session state
     if 'comment' not in st.session_state:
         df_comment = pd.read_csv('comment.csv')
         st.session_state.comment = df_comment
+    #or if 'commment' is existed then restore the csv comment from the session state 
     else:
         df_comment = st.session_state.comment
     
@@ -35,10 +36,10 @@ def main():
     
     #rendering comment section
     with st.expander('💬 Open comments'):
-       
+        #formating the output of comments and replies
         COMMENT_TEMPLATE = '**{}** - {}\n\n|&nbsp;&nbsp;&nbsp;&nbsp;{}'
         REPLY_TEMPLATE = '\n\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;**Reply**: {} - {}'
-        
+        #iterate each comment in the dataset and render it on the web page 
         for _, comment in df_comment.iterrows():
             str = COMMENT_TEMPLATE.format(comment['Name'],comment['Date'],comment['Comment']) +\
                   ('' if pd.isnull(comment['Reply']) else REPLY_TEMPLATE.format(comment['Reply'],comment['Reply Date']))
@@ -47,23 +48,27 @@ def main():
         if st.session_state.just_posted:
             st.success("☝️ Your comment was successfully posted.") 
             st.session_state.just_posted = False
-        
-        #adding new comments
-        st.write('***Add your own comment***')
+    
+        st.write('***Add your comment***')
         
         with st.form('comment_section', clear_on_submit=True):
+            #adding text inputs for name and comment, and a submit button
             name = st.text_input('Name')
             comment = st.text_area('Comment')
             submit = st.form_submit_button('Add comment')
                   
             if submit and name!= '' and comment!='':
+                #getting server local time
                 date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+                #adding new comment to the comment dataframe
                 df_comment = df_comment.append({'Name':name,'Comment':comment,'Date':date}, ignore_index=True)
+                #overwriting the dataframe into csv comment file
                 df_comment.to_csv('comment.csv',index=False)
+                #update the session state to flag a new comment is justed added
                 if st.session_state.just_posted == False:
                     st.session_state.just_posted = True
                 
-                #delete the state and force to refresh the comment - use callback function
+                #delete the session state to force to refresh all comments
                 del st.session_state.comment
                 st.experimental_rerun()
     
